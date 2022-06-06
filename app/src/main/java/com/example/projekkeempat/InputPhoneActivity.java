d727eb2aba0c9a2b6ae8afc80558f359b3d2aac0
rsp23 <radescaputra23@gmail.com>
2022-06-06 16:14:31 +0700

    first commit

---
+++ app/src/main/java/com/example/projekkeempat/InputPhoneActivity.java
@@ -0,0 +1,14 @@
+package com.example.projekkeempat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputPhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_phone);
+    }
+}
---
+++ app/src/main/java/com/example/projekkeempat/SearchLocationActivity.java
@@ -0,0 +1,16 @@
+package com.example.projekkeempat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_location);
+
+
+    }
+}
