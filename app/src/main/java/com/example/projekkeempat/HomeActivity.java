cce7fda2460a63c6ad3af61c4b57dc7eb6f7561f
“Wandhie <wandhie.dimyati@pactindo.com>
2022-06-13 21:20:16 +0700

    add routing java

--- app/src/main/java/com/example/projekkeempat/HomeActivity.java
+++ app/src/main/java/com/example/projekkeempat/HomeActivity.java
@@ -2,13 +2,19 @@ package com.example.projekkeempat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button btnSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnSearch =findViewById(R.id.btnSaveNumber);
+        btnSearch.setOnClickListener(view -> {
+            startActivity(new Intent(this, SignUpSuccesActivity.class));
+        });
     }
 }
